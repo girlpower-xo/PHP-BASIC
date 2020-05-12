40866e28b168bac4776735d8b4439dfbf3fc1ffb
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-12 11:59:44 +0200

    opdracht 9 done

---
+++ 2-Variabelen-en-datatype/taak09/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+    $voornaam = "Iris";
+    $leeftijd = 19;
+    echo "Jouw naam is " . $voornaam . " en je bent " . $leeftijd . " jaar oud.";
+?>
+
+</body>
+</html>
