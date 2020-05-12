e3c2856358c78e8cf940ab7ed223e9b4bd2ec9a3
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-12 11:55:48 +0200

    opdracht 8 af

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+$voornaam1 = "tygo";
+$voornaam2 = "jochem";
+$voornaam3 = "sam";
+
+echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+
+?>
+
+</body>
+</html>
