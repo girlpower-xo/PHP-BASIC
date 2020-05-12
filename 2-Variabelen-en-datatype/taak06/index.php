2d9b4dc385b2a627887fcdbc36f529314bed3be7
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-12 11:25:28 +0200

    opdrahct 6 done

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+<?php
+$string = "hallo";
+$integer = 10;
+$float = 1.9;
+$boolean = true;
+$null = null;
+
+echo gettype($string);
+echo "<br>";
+echo gettype($integer);
+echo "<br>";
+echo gettype($float);
+echo "<br>";
+echo gettype($boolean);
+echo "<br>";
+echo gettype($null);
+echo "<br>";
+?>
+
+</body>
+</html>
