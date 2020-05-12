31cd757ca82fbdc06123af6574c2948e62232b4c
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-12 14:23:01 +0200

    opdracht 10 done

---
+++ 2-Variabelen-en-datatype/taak10/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+     $item = "<li>pindakaas</li>";
+     $item .= "<li>appelstroop</li>";
+     $item .= "<li>kaas</li>";
+
+     echo $item;
+?>
+
+</body>
+</html>
