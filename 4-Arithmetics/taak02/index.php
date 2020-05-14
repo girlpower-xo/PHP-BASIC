45c1959b0d22976567dbb31963c11decc95e8dd5
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-14 12:03:44 +0200

    taak 2 done

---
+++ 4-Arithmetics/taak02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+    $getal1 = 14;
+   echo $getal1 += 10;
+   echo "<br>";
+   echo $getal1 -= 10;
+   echo "<br>";
+   echo $getal1 *= 10;
+   echo "<br>";
+   echo $getal1 /= 10;
+    
+?>
+
+    
+</body>
+</html>
