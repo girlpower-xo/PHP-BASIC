87b8611bf892325967684e1eca2be02aceabd751
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-14 11:44:51 +0200

    taak 1 done

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+    $myMovies = array("sneeuwwitje", "peter pan", "arial");
+    
+   echo "<li>" , $myMovies[0] , "</li>" ;
+   echo "<li>" , $myMovies[1] , "</li>" ;
+   echo "<li>" , $myMovies[2] , "</li>" ;
+
+    
+?>
+
+    
+</body>
+</html>
