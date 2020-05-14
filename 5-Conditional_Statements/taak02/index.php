55fdda7dfb3bb1689b63a35012d01a52b894b62a
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-14 13:06:27 +0200

    taak 2 done

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+   $test1 = 10;
+   $test2 = 6;
+
+   if($test1 == $test2) {
+    echo "gelijk!";
+}
+
+echo "<br>";
+   
+if($test1 != $test2) {
+    echo "ongelijk!";
+}
+echo "<br>";
+
+if($test1 === $test2) {
+    echo "identiek!";
+}
+echo "<br>";
+
+if($test1 > $test2) {
+    echo "test 1 is groter dan test 2";
+}
+echo "<br>";
+
+if($test1 < $test2) {
+    echo "test 1 is kleiner dan test 2";
+}
+echo "<br>";
+
+if($test1 >= $test2) {
+    echo "test1 is groter of gelijk aan test2";
+}
+echo "<br>";
+if($test1 <= $test2) {
+    echo "test1 is kleiner of gelijk aan test2";
+}
+
+
+
+?>
+
+    
+</body>
+</html>
