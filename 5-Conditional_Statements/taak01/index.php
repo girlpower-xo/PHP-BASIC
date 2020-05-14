829179fde657502a4490402414394f7ade77e66f
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-14 12:56:08 +0200

    taak1 done

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+   $test1 = 12;
+   $test2 = "gewoon tekst";
+    
+   var_dump($test1 == $test2);
+   var_dump($test1 != $test2);
+   var_dump($test1 > $test2);
+   var_dump($test1 < $test2);
+   var_dump($test1 <> $test2);
+   var_dump($test1 >= $test2);
+   var_dump($test1 <= $test2);
+   
+?>
+
+    
+</body>
+</html>
