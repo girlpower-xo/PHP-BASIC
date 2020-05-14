f53135d033a8e93e6c04e34c2ee9be28ff087b6e
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-14 11:51:32 +0200

    taak 2 done

---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+    $myDogNames = ['snoesje' , 'pluisje' , 'baco'];
+
+    $myDogNames[1]= 'mickey';
+     echo $myDogNames[0];
+     echo $myDogNames[1];
+      echo $myDogNames[2];
+
+      
+
+?>
+    
+</body>
+</html>
