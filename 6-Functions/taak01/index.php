d1d3ca307dab3074ce4879cb083b421e79d258de
Iris Petersen <iris.petersen@student.rocva.nl>
2020-05-19 10:21:12 +0200

    taak 1 done

---
+++ 6-Functions/taak01/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+  function zetTweeWoordenAanElkaar(){
+
+    $woordje = "hallo";
+    $zinnetje = "mooie blauwe planeet!";
+    $heleZin = $woordje . $zinnetje;
+    
+    echo $heleZin;
+}
+
+
+zetTweeWoordenAanElkaar();
+?>
+
+    
+</body>
+</html>
